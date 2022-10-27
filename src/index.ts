5924ce1363b216c0f141bf13106d759159597ca2
“chenqianhe” <1278095698@qq.com>
2022-10-27 23:45:46 +0800

    :sparkles: enable front-end and back-end connectivity

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,38 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
+import { requestAPI } from './handler';
+
 /**
  * Initialization data for the codegen-paddle extension.
  */
 const plugin: JupyterFrontEndPlugin<void> = {
   id: 'codegen-paddle:plugin',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
+  activate: async (app: JupyterFrontEnd) => {
     console.log('JupyterLab extension codegen-paddle is activated!');
+
+    // GET request
+    try {
+      const data = await requestAPI<any>('hello');
+      console.log(data);
+    } catch (reason) {
+      console.error(`Error on GET /codegen-paddle-backend/hello.\n${reason}`);
+    }
+
+    // POST request
+    const dataToSend = { name: 'George' };
+    try {
+      const reply = await requestAPI<any>('hello', {
+        body: JSON.stringify(dataToSend),
+        method: 'POST'
+      });
+      console.log(reply);
+    } catch (reason) {
+      console.error(
+        `Error on POST /codegen-paddle-backend/hello ${dataToSend}.\n${reason}`
+      );
+    }
   }
 };
 
